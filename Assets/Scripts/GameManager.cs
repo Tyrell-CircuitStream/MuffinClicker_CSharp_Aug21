c0a5ca73f94c24e06da321a6e071b0d81fd3b1dc
Tyrell-CircuitStream <tyrell@circuitstream.com>
2021-09-03 10:29:09 -0700

    Added save and load functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,17 @@ using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+public struct SaveData
+{
+    public int totalClicks;
+    public int pointsPerClick;
+}
 
 public class GameManager : MonoBehaviour
 {
     // Score
     private int totalClicks = 0;
-    [SerializeField] private int pointPerClick = 1;
+    [SerializeField] private int pointsPerClick = 1;
 
     // Floating Text
     [SerializeField] private GameObject floatingTextPrefab;
@@ -18,22 +23,43 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private HeaderUI headerUI = null;
 
+
+    private void Awake()
+    {
+        LoadGame();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         headerUI.UpdateUI(totalClicks);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            totalClicks = 0;
+            pointsPerClick = 1;
+            headerUI.UpdateUI(totalClicks);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
     public void OnMuffinButtonClicked()
     {
         // Increasing the score
-        totalClicks = totalClicks + pointPerClick;
+        totalClicks = totalClicks + pointsPerClick;
 
         // Update UI
         headerUI.UpdateUI(totalClicks);
 
         // Create the floating text notification
-        CreateFloatingText("+" + pointPerClick);
+        CreateFloatingText("+" + pointsPerClick);
     }
 
     private void CreateFloatingText(string message)
@@ -59,4 +85,49 @@ public class GameManager : MonoBehaviour
 
         return new Vector3(x, y);
     }
+
+    private void SaveGame()
+    {
+        Debug.Log("Saving Game...");
+
+        // Create the save data object
+        SaveData saveData = new SaveData();
+
+        // Populate the save data object with the game's current state
+        saveData.totalClicks = totalClicks;
+        saveData.pointsPerClick = pointsPerClick;
+
+        // Convert the save data object into JSON (Serialize it!)
+        string saveJSON = JsonUtility.ToJson(saveData);
+        Debug.Log("Save JSON: " + saveJSON);
+
+        // Save the JSON to PlayerPrefs
+        PlayerPrefs.SetString("savegame", saveJSON);
+    }
+
+    private void LoadGame()
+    {
+        Debug.Log("Loading Game...");
+
+        // Load the JSON to PlayerPrefs
+        string saveJSON = PlayerPrefs.GetString("savegame", "{}");
+
+        // Convert the JSON into a save data object (Deserialize it!)
+        SaveData saveData = JsonUtility.FromJson<SaveData>(saveJSON);
+
+        // Populate the game's current state with the save data object 
+        totalClicks = saveData.totalClicks;
+
+        if (saveData.pointsPerClick == 0)
+        {
+            pointsPerClick = 1;
+        }
+        else
+        {
+            pointsPerClick = saveData.pointsPerClick;
+        }
+
+    }
+
+
 }
